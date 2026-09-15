13558ee0aa4013357c344b624edbfd8d84d6dd41
yixin <44286866+huyixin1@users.noreply.github.com>
2026-09-15 20:20:35 +0800

    Time: 4 ms (82.95%) | Memory: 19.6 MB (34.75%) - LeetSync

--- solutions-2round/39-combination-sum/combination-sum.py
+++ solutions-2round/39-combination-sum/combination-sum.py
@@ -1,30 +1,23 @@
 class Solution:
     def combinationSum(self, candidates: list[int], target: int) -> list[list[int]]:
+        candidates.sort() # pre cut the tree
         res = []
         path = []
-        def dfs(start):
-            s = 0
-            for i in path:
-                s += i
-            if s > target: # invalid choice
-                return
-            if s == target: 
+        def dfs(start, remain):
+            # s = 0
+            # for i in path:
+            #     s += i
+            # calculate sum everytime, better to keep it as a parameter pass with the func
+            if remain == 0: 
                 res.append(path[:])
                 return
 
-            for start in range(len(candidates)):
-                if candidates[start] <= target:
-                    path.append(candidates[start])
-                    dfs(start)
+            for i in range(start, len(candidates)): # only choose the nums next
+                if candidates[i] > remain: break
+                if candidates[i] <= target:
+                    path.append(candidates[i])
+                    dfs(i, remain - candidates[i])
                     path.pop()
 
-        dfs(0)
-        # ① 对每个内部列表排序，再转元组
-        tuples = [tuple(sorted(x)) for x in res]
-
-        # ② 用 set 去重
-        unique_tuples = set(tuples)
-
-        # ③ 转回列表
-        unique_lists = [list(t) for t in unique_tuples]
-        return unique_lists
+        dfs(0, target)
+        return res
